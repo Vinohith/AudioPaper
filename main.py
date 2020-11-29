e45dd3aa48901db8c2d34b49d401844af0366ad9
Vinohith <vinohith1998@gmail.com>
2020-11-29 17:09:49 +0530

    Code cleanup

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ else:
 doc = fitz.open(fname)
 page_count = len(doc)
 
-# storage for page display lists
 dlist_tab = [None] * page_count
 
 title = "PyMuPDF display of '%s', pages: %i" % (fname, page_count)
@@ -32,11 +31,8 @@ curr_dir = os.getcwd()
 final_dir = os.path.join(curr_dir, 'software')
 if not os.path.exists(final_dir):
     os.makedirs(final_dir)
-# print(curr_dir)
-# print(final_dir)
 image_dir = glob.glob(final_dir)
 for file_name in os.listdir(final_dir):
-    # print(file_name)
     filepath = os.path.join(final_dir, file_name)
     os.chmod(filepath, 0o777)
     os.remove(filepath)
@@ -71,43 +67,29 @@ def get_page(pno, zoom=0):
         pix = dlist.getPixmap(alpha=False, matrix=mat, clip=clip)
     output = os.path.join(final_dir, r"image_to_read.png")
     pix.writePNG(output)
-    # print(output)
-    return output  # return the PNG image
+    return output
 
 
 def get_text(pno, start_pt, end_pt):
-    # dlist = dlist_tab[pno]  # get display list
-    # if not dlist:  # create if not yet there
-    #     dlist_tab[pno] = doc[pno].getDisplayList()
-    #     dlist = dlist_tab[pno]
     page = doc.loadPage(pno)
-    # text = page.getText()
     text = page.getTextbox(fitz.Rect(start_pt[0], start_pt[1], end_pt[0], end_pt[1]))
-    # print(start_pt[0], start_pt[1], end_pt[0], end_pt[1])
-    # print(text)
-    # col = fitz.utils.getColor("PURPLE")
-    # page.drawRect(fitz.Rect(start_pt[0], start_pt[1], end_pt[0], end_pt[1]), color=col, fill=col, overlay=False)
-    # t = page.getTextbox(fitz.Rect(start_pt[0], start_pt[1], end_pt[0], end_pt[1]))
-    # zoom_x = 2.0
-    # zoom_y = 2.0
-    # mat = fitz.Matrix(zoom_x,zoom_y)
-    # pix = page.getPixmap(matrix=mat)
-    # output = os.path.join(final_dir, r"image_to_read.png")
-    # pix.writePNG(output)
     pattern = "\[\d+\]\,*\ *"
     pattern2 = "\[\d+\]\,*\."
     x = re.findall(pattern, text)
-    # print(x)
     text = re.sub(pattern, "", text)
     text = re.sub(pattern2, ".", text)
-    # print(text)
     return text
 
 
+def cleanup():
+    for file_name in os.listdir(final_dir):
+        filepath = os.path.join(final_dir, file_name)
+        os.chmod(filepath, 0o777)
+        os.remove(filepath)
+
 
 cur_page = 0
-data = get_page(cur_page)  # show page 1 for start
-# image_elem = sg.Image(data=data)
+data = get_page(cur_page) 
 
 goto = sg.InputText(str(cur_page + 1), size=(5, 1))
 
@@ -130,7 +112,7 @@ layout = [
     graph_bottom_left=(0, 792),
     graph_top_right=(612, 0),
     key="-GRAPH-",
-    change_submits=True,  # mouse click events
+    change_submits=True,
     background_color='lightblue',
     drag_submits=True),
     sg.Frame(layout=[
@@ -144,10 +126,11 @@ layout = [
     ],
     [sg.Text(key='info', size=(60, 1))]
 ]
+
+
 my_keys = ("Next", "Next:34", "Prev", "Prior:33", "Top-L", "Top-R",
            "Bot-L", "Bot-R", "MouseWheel:Down", "MouseWheel:Up")
 zoom_buttons = ("Top-L", "Top-R", "Bot-L", "Bot-R")
-
 
 
 window = sg.Window(title, layout,
@@ -158,27 +141,27 @@ dragging = False
 start_point = end_point = prior_rect = None
 
 old_page = 0
-old_zoom = 0  # used for zoom on/off
-# the zoom buttons work in on/off mode.
+old_zoom = 0  
 
 while True:
     event, values = window.read(timeout=100)
     zoom = 0
     force_page = False
     if event == sg.WIN_CLOSED:
+        cleanup()
         break
 
-    if event in ("Escape:27",):  # this spares me a 'Quit' button!
+    if event in ("Escape:27",):
         break
-    if event[0] == chr(13):  # surprise: this is 'Enter'!
+    if event[0] == chr(13):
         try:
-            cur_page = int(values[0]) - 1  # check if valid
+            cur_page = int(values[0]) - 1
             while cur_page < 0:
                 cur_page += page_count
         except:
-            cur_page = 0  # this guy's trying to fool me
+            cur_page = 0
         goto.update(str(cur_page + 1))
-        # goto.TKStringVar.set(str(cur_page + 1))
+
 
     elif event in ("Next", "Next:34", "MouseWheel:Down"):
         cur_page += 1
@@ -193,13 +176,11 @@ while True:
     elif event == "Bot-R":
         zoom = 4
 
-    # sanitize page number
-    if cur_page >= page_count:  # wrap around
+    if cur_page >= page_count:
         cur_page = 0
-    while cur_page < 0:  # we show conventional page numbers
+    while cur_page < 0:
         cur_page += page_count
 
-    # prevent creating same data again
     if cur_page != old_page:
         zoom = old_zoom = 0
         force_page = True
@@ -213,17 +194,14 @@ while True:
             force_page = True
 
     if force_page:
-        # print(cur_page)
         data = get_page(cur_page, zoom)
         graph.draw_image(data, location=(0,0))
         old_page = cur_page
     old_zoom = zoom
 
-    # update page number field
     if event in my_keys or not values[0]:
         goto.update(str(cur_page + 1))
-        # goto.TKStringVar.set(str(cur_page + 1))
-    if event == "-GRAPH-":  # if there's a "Graph" event, then it's a mouse
+    if event == "-GRAPH-":
         x, y = values["-GRAPH-"]
         if not dragging:
             start_point = (x, y)
@@ -235,25 +213,20 @@ while True:
         if None not in (start_point, end_point):
             prior_rect = graph.draw_rectangle(start_point, end_point, line_color='red')
 
-    elif event.endswith('+UP'):  # The drawing has ended because mouse up
+    elif event.endswith('+UP'):
         info = window["info"]
         info.update(value=f"grabbed rectangle from {start_point} to {end_point}")
-        # print(start_point, end_point)
         text = get_text(cur_page, start_point, end_point)
-        start_point, end_point = None, None  # enable grabbing a new rect
+        start_point, end_point = None, None
         dragging = False
-        # print(text)
         tts = gTTS(text=text, lang='en', slow=False)
         speech_output = os.path.join(final_dir, r"speech.mp3")
         tts.save(speech_output)
         print("Saved")
-        # playback the speech
+
     if event == "Speak":
         mixer.music.load(speech_output)
         mixer.music.play()
-    # wait for playback to end
-    # while mixer.music.get_busy():
-        # time.sleep(.1)
     if event == "Pause":
         mixer.music.pause()
     if event == "Unpause":
@@ -264,139 +237,7 @@ while True:
             os.remove(speech_output)
         except:
             pass
-    # if event == "Pause":
-    #     mixer.music.pause()
-    # if event == "Unpause":
-    #     mixer.music.unpause()
+
     mixer.stop()
-    # try:
-    #     os.remove(speech_output)
-    # except:
-    #     pass
-    
-    # if None not in (start_point, end_point):
-    #     print("not none")
-    # tts = gTTS(text=text, lang='en', slow=False)
-    # speech_output = os.path.join(final_dir, r"speech.mp3")
-    # tts.save(speech_output)
-    # # playback the speech
-    # mixer.music.load(speech_output)
-    # # mixer.music.load('speech0.mp3')
-    # mixer.music.play()
-    # # wait for playback to end
-    # while mixer.music.get_busy():
-    #     time.sleep(.1)
-    # mixer.stop()
 
-
-# import PySimpleGUI as sg
-# import string
-# import os
-# import glob
-# import fitz
-# import re
-# from PIL import Image
-# import pytesseract
-
-# def get_page_numbers(value):
-#     # print(value)
-#     value = value.translate({ord(c): None for c in string.whitespace})
-#     # print(value)
-#     if '-' in value:
-#         first_page_number = int(value.split('-')[0])
-#         last_page_number = int(value.split('-')[1])
-#     else:
-#         first_page_number = int(value)
-#         last_page_number = -1
-#     return first_page_number, last_page_number
-
-
-# def main():
-#     # layout = [[sg.Text("Choose file"), sg.Input(), sg.FileBrowse()],
-#     #           [sg.Text("Enter Pasge Numbers seperated by - "), sg.InputText()],
-#     #           [sg.Button("OK"), sg.Button("CANCEL")]]
-#     # window = sg.Window("Input", layout)
-#     # while True:
-#     #     event, values = window.read()
-#     #     # print(event, values)
-#     #     if event == sg.WIN_CLOSED or event == "CANCEL":
-#     #         print("Exiting")
-#     #         exit()
-#     #         window.close()
-#     #     if event == "OK":
-#     #         # if values[0] == "":
-#     #         #     sg.Popup("Enter PDF file")
-#     #         # elif values[1] == "":
-#     #         #     sg.Popup("Enter page number(s)")
-#     #         # elif values[0] != "" and values[1] != "":
-#     #         #     # print("You entered : ", values[0], " ", values[1])
-#     #         #     break
-#     #         if values[0] != "" or values[1] != "":
-#     #             break
-#     # window.close()
-#     # print("You entered : ", values[0], " ", values[1])
-#     # first_page_no, last_page_no = get_page_numbers(values[1])
-#     # print("1st page number : ", first_page_no)
-#     # print("Last page number : ", last_page_no)
-
-#     curr_dir = os.getcwd()
-#     final_dir = os.path.join(curr_dir, 'software')
-#     if not os.path.exists(final_dir):
-#         os.makedirs(final_dir)
-#     print(curr_dir)
-#     print(final_dir)
-#     image_dir = glob.glob(final_dir)
-#     for file_name in os.listdir(final_dir):
-#         # print(file_name)
-#         filepath = os.path.join(final_dir, file_name)
-#         os.chmod(filepath, 0o777)
-#         os.remove(filepath)
-    
-#     pdf_file = fitz.open("test_paper.pdf")
-#     first_page_no, last_page_no = 2, -1
-#     if last_page_no == -1:
-#         page = pdf_file.loadPage(first_page_no - 1)
-#         # mat = 
-#         # font_list = pdf_file.getPageFontList(first_page_no)
-#         # text = pdf_file.getPageText(first_page_no)
-#         text = page.getText()
-#         # print(page.links())
-#         pattern = "\[\d+\]"
-#         links = page.getLinks()
-#         t = page.getText()
-#         # x = re.findall(pattern, t)
-#         f = page.getFontList()
-#         print(page.rect)
-#         col = fitz.utils.getColor("PURPLE")
-#         page.drawRect(fitz.Rect(44.0, 374.0, 309.0, 681.0), color=col, fill=col, overlay=False)
-#         t = page.getTextbox(fitz.Rect(44.0, 374.0, 309.0, 681.0))
-#         zoom_x = 2.0
-#         zoom_y = 2.0
-#         mat = fitz.Matrix(zoom_x,zoom_y)
-#         pix = page.getPixmap(matrix=mat)
-#         output = os.path.join(final_dir, r"image_to_read.png")
-#         pix.writePNG(output)
-    
-#     print(t)
-#     # for i in t[0]:
-#     #     print(t)
-#     # print(x)
-#     # print(f)
-
-#     # for file in os.listdir(final_dir):
-#     #     data = pytesseract.image_to_string(Image.open(os.path.join(final_dir,file)),lang="eng")
-#     #     data = data.replace("|","I") # For some reason the image to text translation would put | instead of the letter I. So we replace | with I
-#     #     data = data.split('\n')
-#     #     mytext.append(data)
-
-#     # print(mytext)
-
-#     # print(font_list)
-#     # print(text)
-#     # for link in links:
-#     #     print(link)
-
-
-
-# if __name__ == "__main__":
-#     main()
+window.close()
